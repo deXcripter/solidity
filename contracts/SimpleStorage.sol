e826f8e35f2f551886ef7f9788914c84c384bd1d
Johnpaul Nnaji <indisputable.jay@gmail.com>
2024-10-04 09:15:05 +0100

    more son solidty basics

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -1,14 +1,19 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity 0.8.8;
 
 contract SimpleStorage {
-    uint256 public storedData;
+ 
+    uint256 public favoriteNumber;
 
-    function set(uint256 x) public {
-        storedData = x;
+    function store (uint256 _favoritenumber) public {
+        favoriteNumber = _favoritenumber;
+        favoriteNumber = favoriteNumber + 1;
+        favoriteNumber = favoriteNumber + 1;
+        favoriteNumber = favoriteNumber + 1;
+        favoriteNumber = favoriteNumber + 1;
     }
 
-    function get() public view returns (uint256) {
-        return storedData;
-    }
+    // visibility specifiers - public - private - external - (internal)
+
 }
+    // 0xd2a5bC10698FD955D1Fe6cb468a17809A08fd005
