f2b0bb31ea24e6d8ab46bca4afbf2706c9995b97
Johnpaul Nnaji <indisputable.jay@gmail.com>
2024-10-11 01:19:43 +0100

    arrays and struct

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -1,11 +1,9 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.28;
+pragma solidity 0.8.26;
 
 contract SimpleStorage {
  
     uint256 public myFavoriteNumber;
-
-    uint256[] listOfFavouredNumbers;
 
     struct Person {
         uint256 favouriteNumber;
@@ -13,5 +11,19 @@ contract SimpleStorage {
         bool adult;
     }
 
-    Person public John = Person(30, "Johnpaul", true);
+    // this is stressful
+   // Person public John = Person(30, "Johnpaul", true);
+   // Person public Paul = Person(30, "Paul", true);
+   // Person public Favour = Person(30, "Favour", true);
+
+    // dynamic array
+    Person[] public listOfPeople;
+
+    // this is a static array but capped at size 3
+    Person[3] public listOfThreePeople;
+
+    function addPerson (string memory name, uint256 _favouriteNumber) public  {
+        Person memory newPerson = Person({name: name, favouriteNumber: _favouriteNumber, adult: true});
+        listOfPeople.push(newPerson);
+    }
 }
