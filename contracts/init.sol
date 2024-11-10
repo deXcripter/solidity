90b23e4a56d8f37be21c3cd3861b0b86936f4ff3
johnpaul <dexcripter@gmail.com>
2024-11-10 07:26:14 +0100

    working with structs

--- contracts/init.sol
+++ contracts/init.sol
@@ -2,9 +2,20 @@
 pragma solidity ^0.8.26; 
 
 contract SimpleStorage {
-        bool hasFavoriteNUmber = true; 
-        int256 favoriteInt = -88;
-        string favoriteNumberInText = 'eighty-eight';
-        address myAddress = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
-        bytes32 myBytes = "hello world";
+       uint256 favoriteNumber;
+
+       struct Person {
+        uint256 personFavoriteNumber;
+        string name;
+       }
+
+       Person public john = Person(23, "Johnpaul");
+
+       function store(uint256 _favoriteNumber) public {
+            favoriteNumber = _favoriteNumber;
+         }
+
+        function retrieve() public view returns (uint256) {
+          return favoriteNumber;
+        }
 }
