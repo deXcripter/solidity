a4527eafc66c937f20d38659f149d4331f986641
johnpaul <dexcripter@gmail.com>
2024-11-10 06:33:40 +0100

    types

--- contracts/init.sol
+++ contracts/init.sol
@@ -2,5 +2,9 @@
 pragma solidity ^0.8.26; 
 
 contract SimpleStorage {
-        uint256 favoriteNUmber = 88;
+        bool hasFavoriteNUmber = true; 
+        int256 favoriteInt = -88;
+        string favoriteNumberInText = 'eighty-eight';
+        address myAddress = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+        bytes32 myBytes = "hello world";
 }
