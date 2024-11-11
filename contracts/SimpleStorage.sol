72dec3fae93fec4916b91af58a56f3badcbe90e7
johnpaul <dexcripter@gmail.com>
2024-11-11 17:47:12 +0100

    my own contract

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -2,28 +2,18 @@
 pragma solidity 0.8.26;
 
 contract SimpleStorage {
- 
-    uint256 public myFavoriteNumber;
-
+    
     struct Person {
-        uint256 favouriteNumber;
         string name;
-        bool adult;
+        int256 age; 
+        bool male;
     }
 
-    // this is stressful
-   // Person public John = Person(30, "Johnpaul", true);
-   // Person public Paul = Person(30, "Paul", true);
-   // Person public Favour = Person(30, "Favour", true);
+    Person[] public listOfregisteredPeople;
 
-    // dynamic array
-    Person[] public listOfPeople;
 
-    // this is a static array but capped at size 3
-    Person[3] public listOfThreePeople;
-
-    function addPerson (string memory name, uint256 _favouriteNumber) public  {
-        Person memory newPerson = Person({name: name, favouriteNumber: _favouriteNumber, adult: true});
-        listOfPeople.push(newPerson);
+    function addPerson (string memory name, int number, bool male) public {
+        Person memory newPerson = Person({name: name, age: number, male: male});
+        listOfregisteredPeople.push(newPerson);        
     }
 }
