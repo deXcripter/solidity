8ec6fc2bc4e5c10e993813ca07d6fd6d1ac140a1
johnpaul <dexcripter@gmail.com>
2024-11-12 07:32:03 +0100

    sending payments to a contract

--- fund-me/contracts/FundMe.sol
+++ fund-me/contracts/FundMe.sol
@@ -2,10 +2,14 @@
 // Withdraw funds 
 // Set a minimum funding value is USD
 
-// SPDF-License-dentifier: MIT
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
 contract FundMe {
-    function fund() public {}
+    function fund() public payable {
+        // allow users to send money
+        // have a minimum $fee
+        require(msg.value > 1e18, "did'nt send enough ETH");
+    }
     // function withdraw() public {}
 }
