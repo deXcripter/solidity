bf86b5a044d41adfa2f232e3ad73c9d1b6031351
johnpaul <dexcripter@gmail.com>
2024-11-12 07:24:44 +0100

    program structure

---
+++ fund-me/contracts/FundMe.sol
@@ -0,0 +1,11 @@
+// Get finds from users
+// Withdraw funds 
+// Set a minimum funding value is USD
+
+// SPDF-License-dentifier: MIT
+pragma solidity ^0.8.18;
+
+contract FundMe {
+    function fund() public {}
+    // function withdraw() public {}
+}
