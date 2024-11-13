d9fc0b74853ac1acd2e04b4a41513bda16676727
johnpaul <dexcripter@gmail.com>
2024-11-13 23:32:34 +0100

    chore: forge init

--- fund-me/contracts/FundMe.sol
+++ fund-me/contracts/FundMe.sol
@@ -1,15 +1,25 @@
 // Get finds from users
-// Withdraw funds 
+// Withdraw funds
 // Set a minimum funding value is USD
 
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
 contract FundMe {
+    uint256 public minimumUSD = 5;
+
     function fund() public payable {
         // allow users to send money
         // have a minimum $fee
         require(msg.value > 1e18, "did'nt send enough ETH");
     }
+
     // function withdraw() public {}
+
+    function getPrice() public {
+        // address - 0x694AA1769357215DE4FAC081bf1f309aDC325306
+        // ABI -
+    }
+
+    function getConversionRate() public {}
 }
