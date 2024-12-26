3e26d279fad5e1eee20211158f91f754800f0f60
johnpaul <dexcripter@gmail.com>
2024-12-26 09:32:34 +0100

    refactoring the cbase

--- fund-me/test/FundMeTest.t.sol
+++ fund-me/test/FundMeTest.t.sol
@@ -2,24 +2,29 @@
 
 pragma solidity ^0.8.19;
 
-import { Test, console } from "forge-std/Test.sol";
+import {Test} from "forge-std/Test.sol";
 import {FundMe} from "../src/FundMe.sol";
+import {DeployFundMe} from "../script/DeployFundMe.s.sol";
 
 contract FundMeTest is Test {
+    FundMe fundMe;
 
-  FundMe fundMe;
+    function setUp() external {
+        DeployFundMe deployFundMe = new DeployFundMe();
+        fundMe = deployFundMe.run();
+    }
 
+    function testMinimumDollarsIsFive() public {
+        assertEq(fundMe.MINIMUM_USD(), 5e18);
+    }
 
-  function setUp() external {
-    fundMe = new FundMe();
-  }
+    function testConfirmOwnerIsMsgSender() public {
+        assertEq(fundMe.i_owner(), msg.sender);
+    }
 
-  function testMinimumDollarsIsFive() public {
-    assertEq(fundMe.MINIMUM_USD(), 5e18);
-  }
+    function testPriceFeedIsAccurate() public {
+        uint256 version = fundMe.getVersion();
 
-  function testConfirmOwnerIsMsgSender() public {
-    assertEq(fundMe.i_owner(), address(this));
-  }
-
+        assertEq(version, 4);
+    }
 }
