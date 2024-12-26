8af88d5b750813080793c2dc2a6ce465a734b34b
johnpaul <dexcripter@gmail.com>
2024-12-26 07:54:28 +0100

    deploying via smart contract

--- fund-me/src/FundMe.sol
+++ fund-me/src/FundMe.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.18;
 import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 import {PriceConverter} from "./PriceConverter.sol";
 
-error FundMe__NotOwner(); // to easily tell which contacct the error came from. 
+error FundMe__NotOwner(); // to easily tell which contact the error came from 
 
 contract FundMe {
     using PriceConverter for uint256;
