28a08bef0b8917699b922ec796a9d5452c1d7783
johnpaul <dexcripter@gmail.com>
2025-02-01 22:00:30 +0100

    raffle draw

--- smart-contract-lottery/src/Raffle.sol
+++ smart-contract-lottery/src/Raffle.sol
@@ -1,17 +1,50 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
+/**
+ * @title Raffle
+ * @author deXcripter
+ * @notice
+ * @dev
+ */
 contract Raffle {
-    uint256 private immutable i_EntranceFee;
+    error Raffle__NotEnoughEthSent();
 
-    constructor(uint256 entranceFee) {
+    uint256 private immutable i_EntranceFee;
+    uint256 private immutable i_interval; // duration of the lottery in seconds
+    address payable[] private s_players;
+    uint256 private s_lastTimeStamp;
+
+    /* Events */
+    event Raffle__PlayerEntered(address indexed player);
+
+    constructor(uint256 entranceFee, uint256 interval) {
         i_EntranceFee = entranceFee;
+        i_interval = interval;
+        s_lastTim eStamp = block.timestamp;
     }
 
-    function enterRaffle() public payable {}
+    function enterRaffle() external payable {
+        // require(msg.value >= i_EntranceFee, "Not enough ETH sent");
+        if (msg.value < i_EntranceFee) revert Raffle__NotEnoughEthSent();
+        s_players.push(payable(msg.sender));
+        emit Raffle__PlayerEntered(msg.sender);
+    }
 
-    function selectWinner() public {}
+    /**
+     * @notice Selects a winner from the players list
+     * @dev This function gets a random numner to select a winner
+     */
+    function selectWinner() external {
+        if ((block.timestamp - s_lastTimeStamp) < i_interval) {
+            revert();
+        }
+        // Get our random number from Chainlink VRF
+    }ref
 
+    /**
+     * GETTERS
+     */
     function getEntranceFee() public view returns (uint256) {
         return i_EntranceFee;
     }
