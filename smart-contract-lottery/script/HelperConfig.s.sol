efb3fa5afbf2df2f735e8d16c08572fdcb065f6b
dexcripter <dexcripter@gmail.com>
2025-02-03 14:08:03 +0100

    done with the helper config

--- smart-contract-lottery/script/HelperConfig.s.sol
+++ smart-contract-lottery/script/HelperConfig.s.sol
@@ -3,6 +3,7 @@
 pragma solidity 0.8.19;
 
 import {Script} from "forge-std/Script.sol";
+import {VRFCoordinatorV2_5Mock} from "../lib/chainlink-brownie-contracts/contracts/src/v0.8/vrf/mocks/VRFCoordinatorV2_5Mock.sol";
 
 contract HelperConfig is Script {
     // 0x9DdfaCa8183c41ad55329BdeeD9F6A8d53168B1B
@@ -29,9 +30,19 @@ contract HelperConfig is Script {
     }
 
     function getOrCreateAnvilEthConfig() public returns(NetworkConfig memory) {
+        uint96 MOCK_BASE_FEE = 0.01 ether;
+        uint32 MOCK_GAS_PRICE_LINK = 1e9;
+        int256 MOCK_WEI_PER_UNIT_LINK = 4e15;
+
         if (localNetworkConfig.i_vrfCoordinator != address(0)) {
             return localNetworkConfig;
         }
+
+        vm.startBroadcast();
+        VRFCoordinatorV2_5Mock vrfCoordinator = new VRFCoordinatorV2_5Mock(MOCK_BASE_FEE, MOCK_GAS_PRICE_LINK, MOCK_WEI_PER_UNIT_LINK);
+        vm.stopBroadcast();
+
+        localNetworkConfig = NetworkConfig(0.01 ether, 30, 0, address(vrfCoordinator));
     }
 
     function getSepoliaEthConfig() public pure returns(NetworkConfig memory) {
